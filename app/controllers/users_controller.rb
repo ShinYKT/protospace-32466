2c5c18e44778c6f77111300c90d01659ab02298e
ShinsukeYokote <sy114088@gmail.com>
2020-12-17 16:16:28 +0900

    修正4

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,10 @@
 class UsersController < ApplicationController
+ 
+
   def show
     @user = User.find(params[:id])
-    @prototypes = current_user.prototypes
+    @prototypes = @user.prototypes
   end
 
-
+  
 end
